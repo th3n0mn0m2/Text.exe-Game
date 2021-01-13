afe9cac8120f2caaaca0f690f9cc72f1b1f204bb
th3n0mn0m2 <77366742+th3n0mn0m2@users.noreply.github.com>
2021-01-13 08:26:42 +0800

    Input for start game

---
+++ Python.script.py
@@ -0,0 +1,8 @@
+print('Text.exe - The Adventure')
+print('To start game, write start')
+value = input("Start Game?:\n")
+
+if f'{value}' == 'start':
+    print('game start')
+print('=============================================================')
+print('yay the input works! :)')
